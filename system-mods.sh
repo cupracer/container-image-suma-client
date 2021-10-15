27ed316542ef6dc61804c74d365c13fda7bc7f1b
Thomas Schulte <thomas@cupracer.de>
2021-10-15 06:32:27 +0200

    re-order code and use full paths

--- system-mods.sh
+++ system-mods.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if [ $ENABLE_SSH -eq 1 ];
+then
+	/usr/bin/systemctl enable --now sshd.service
+fi
+
 if [ "$ACTIVATION_KEY" != "" ] && [ "$SUMA_HOSTNAME" != "" ];
 then
 	if ! [ -f /registered ]; 
 	then
-		DELAY=$(shuf -i${MIN_DELAY_SEC}-${MAX_DELAY_SEC} -n1)
+		DELAY=$(/usr/bin/shuf -i${MIN_DELAY_SEC}-${MAX_DELAY_SEC} -n1)
 
 		if [ $DELAY -gt 0 ]; 
 		then
@@ -12,20 +17,15 @@ then
 			sleep $DELAY
 		fi
 
-		curl -Sks "https://${SUMA_HOSTNAME}/pub/bootstrap/${BOOTSTRAP_FILE}" | /bin/bash
+		/usr/bin/curl -Sks "https://${SUMA_HOSTNAME}/pub/bootstrap/${BOOTSTRAP_FILE}" | /bin/bash
 		touch /registered
 	else
 		echo "* Machine already registered."
 	fi
 
 	echo "* Use fake uptime in Salt module"
-	sed -i 's/\/proc\/uptime/\/tmp\/uptime/' /usr/lib/python3.6/site-packages/salt/modules/status.py
+	/usr/bin/sed -i 's/\/proc\/uptime/\/tmp\/uptime/' /usr/lib/python3.6/site-packages/salt/modules/status.py
 else
 	echo "* Registration disabled."
-fi
-
-if [ $ENABLE_SSH -eq 1 ];
-then
-	systemctl enable --now sshd.service
 fi
 
